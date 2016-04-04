3b55d94d54254a72a28ab8117918634278d98f21
blykes <blykes@gmail.com>
2016-04-04 16:11:21 -0400

    Update Registration.java

--- Registration.java
+++ Registration.java
@@ -5,10 +5,18 @@ import java.
 
 public class Registration {
 
+private String userName;
+private String password;
+
 	public Registration() {
 		// TODO Auto-generated constructor stub
+		init();
 		
-		
+	}
+	
+	public Registratation(String userName)
+	{
+		init(registration);
 	}
 
 	public static void main(String[] args) {
