777d530adfea3217bf85b080157a62b9d3d0a111
blykes <blykes@gmail.com>
2016-04-18 15:44:47 -0400

    Create Exeception.java

---
+++ Exeception.java
@@ -0,0 +1,9 @@
+//Tweet exception
+
+public class TweetException extends Exception {
+    private static final long serialVersionUID = 7577136074623618615L;
+    public TweetException(Exception e) {
+        super(e);
+    }
+}
+
